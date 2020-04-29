84d64191dbea72880767e3d1d970abd218b61997
Karri DiPetrillo <kdipetri@github.com>
2020-04-29 14:14:08 -0500

    clean landau and position

--- scripts/landau_ish.py
+++ scripts/landau_ish.py
@@ -4,7 +4,6 @@ import os, sys, re
 import ROOT
 
 setStyle()
-dut=12
 
 #thresholds = [100]
 #thresholds = [50,60,70,80,90,100,110,120]
@@ -22,13 +21,14 @@ infos.append([4 , 0, 3 , 1, "config_212_4_3_14_photek"])
 infos.append([4 , 0, 14, 2, "config_212_4_3_14_photek"])
 
 def label(i,opt):
-    if i==0: 
-        if "hit" in opt: return "Track & hit in strip"
-        else : return "Track in strip"
-    if i==1: return "in 1st adjacent strip"
-    if i==2: return "in 2nd adjacent strip"
-    if i==3: return "in 3rd adjacent strip"
-    return ""
+    label = "Track in " 
+    if "hit" in opt: label = "Track & hit in " 
+    if i==0: label+= "channel 4"
+    elif i==1: label+= "1st adjacent channel"
+    elif i==2: label+= "2nd adjacent channel"
+    elif i==3: label+= "3rd adjacent channel"
+    else : return ""
+    return label 
 
 def get_landau_charge(tree,ch,ch_name,adj_ch,adj_name,output,opt=""): 
     
@@ -89,14 +89,13 @@ def plot_overlay_charge(infos,output,opt=""):
 def get_landau(tree,ch,ch_name,adj_ch,adj_name,output,opt=""): 
     
     histname = "h_amp_ch_{}_track{}_in_{}".format(ch_name,opt,adj_name)
-    hist = ROOT.TH1D(histname,";amplitude [mV]",70,0,1000)
+    hist = ROOT.TH1D(histname,";amplitude [mV]",60,0,1200)
 
     print(ch,ch_name,adj_ch,adj_name)
     track_pos = in_strip(adj_name)
     hit = "amp[%i]>100"%adj_ch 
     sel = "{}&&{}&&{}".format(photek,track_sel,track_pos)
     if "hit" in opt: sel = "{}&&{}&&{}".format(track_sel,track_pos,hit)
-    
     
     tree.Project(histname,"amp[%i]"%ch,sel)
 
@@ -115,7 +114,8 @@ def plot_overlay(infos,output,opt=""):
     c = ROOT.TCanvas("c","",800,800)
     c.SetLeftMargin(0.18)
     c.SetBottomMargin(0.15)
-    leg = ROOT.TLegend(0.5,0.65,0.88,0.88)
+    leg = ROOT.TLegend(0.3,0.65,0.88,0.88)
+    leg.SetBorderSize(0)
     for i,info in enumerate(infos):  
         
         ch_name = info[0]
@@ -124,10 +124,10 @@ def plot_overlay(infos,output,opt=""):
         histname = "h_amp_ch_{}_track{}_in_{}".format(ch_name,opt,adj_name)
         hist = output.Get(histname)
         hist.Scale(1.0/hist.Integral(0,-1))
-        hist.SetMaximum(0.4)
+        hist.SetMaximum(0.55)
         hist.SetMinimum(0)
         hist.GetYaxis().SetTitle("Fraction of Events")
-        hist.GetXaxis().SetTitle("Amplitude [mV]")
+        hist.GetXaxis().SetTitle("Channel 4 Amplitude [mV]")
         hist.GetYaxis().SetNdivisions(505)
         hist.GetXaxis().SetNdivisions(505)
         hist.GetYaxis().SetTitleOffset(1.1)
@@ -160,9 +160,9 @@ def make_histos():
         print(ch_name,cfg)
 
         get_landau(tree,ch,ch_name,adj_ch,adj_name,output) 
-        get_landau(tree,ch,ch_name,adj_ch,adj_name,output,"hit") 
-        get_landau_charge(tree,ch,ch_name,adj_ch,adj_name,output) 
-        get_landau_charge(tree,ch,ch_name,adj_ch,adj_name,output,"hit") 
+        #get_landau(tree,ch,ch_name,adj_ch,adj_name,output,"hit") 
+        #get_landau_charge(tree,ch,ch_name,adj_ch,adj_name,output) 
+        #get_landau_charge(tree,ch,ch_name,adj_ch,adj_name,output,"hit") 
     
 
     return 
@@ -172,14 +172,14 @@ def make_plots():
     output = ROOT.TFile.Open("plots/landau/tmp.root")
 
     plot_overlay(infos,output)
-    plot_overlay(infos,output,"hit")
-    plot_overlay_charge(infos,output)
-    plot_overlay_charge(infos,output,"hit")
+    #plot_overlay(infos,output,"hit")
+    #plot_overlay_charge(infos,output)
+    #plot_overlay_charge(infos,output,"hit")
 
     return
 
 # Main
 if __name__ == "__main__":
 
-    make_histos()
+    #make_histos()
     make_plots()
--- scripts/position.py
+++ scripts/position.py
@@ -3,7 +3,7 @@ from common import *
 import os, sys, re
 import ROOT
 
-
+outfile = ROOT.TFile("plots/position/tmp.root","RECREATE")
 def get_position(tree,cfg,ch,ch_name):
     
     minAmp = 110 # for now 
@@ -15,16 +15,31 @@ def get_position(tree,cfg,ch,ch_name):
     ymin = 22 
     ymax = 25
 
-    #track_sel = "ntracks==1&&npix>0&&nback>0"
-    dist = "amp[{}]>{}:y_dut[{}]:x_dut[{}]".format(ch,minAmp,dut,dut)
-    hist = "({},{},{},{},{},{})".format(nx,xmin,xmax,ny,ymin,ymax)
-    sel = "{}&&{}".format(track_sel,photek)
-    
-    c = ROOT.TCanvas()
-    tree.Draw("{}>>{}".format(dist,hist),sel,"PROFCOLZ")
+    name_num = "{}_{}_num_xy".format(cfg,ch_name)
+    name_den = "{}_{}_den_xy".format(cfg,ch_name)
+    hist_num = ROOT.TH2F(name_num,";x [mm];y [mm];Events",nx,xmin,xmax,ny,ymin,ymax)
+    hist_den = ROOT.TH2F(name_den,";x [mm];y [mm];Events",nx,xmin,xmax,ny,ymin,ymax)
+    hist_num.Sumw2()
+    hist_den.Sumw2()
 
+    sel = "{}&&{}".format(track_sel,photek)
+    amp = "amp[%i]>%i && t_peak[%i]-LP2_20[3]>3.0e-9 && t_peak[%i]-LP2_20[3]<5.0e-9"%(ch,minAmp,ch,ch)
+    dist = "y_dut[{}]:x_dut[{}]".format(dut,dut)
     
-    c.Print("plots/position/{}_ch{}_xy.pdf".format(cfg,ch_name))
+    tree.Project(name_num,dist,sel+"&&"+amp,"COLZ")
+    tree.Project(name_den,dist,sel,"COLZ")
+
+    hist = hist_num.Clone("{}_{}_eff_xy".format(cfg,ch_name))
+    hist.Divide(hist_num,hist_den,1,1,"B")
+    hist.GetZaxis().SetRangeUser(0,1)
+    hist.GetZaxis().SetTitle("Efficiency, {} mV".format(minAmp))
+    hist.GetXaxis().SetNdivisions(505)
+    c = ROOT.TCanvas()
+    hist.Draw("COLZ") 
+    c.Print("plots/position/{}_ch{}_eff_xy.pdf".format(cfg,ch_name))
+
+    outfile.cd()
+    hist.Write()
 
     return 
     
